e0be31215447dba7dcd895c21ae5b819dbe5617e
sebwelsch <sebjak1@hotmail.com>
2020-08-25 23:38:09 +0200

    Commit

--- afleveringer/bumleg.py
+++ afleveringer/bumleg.py
@@ -1,12 +1,29 @@
-def checkForBum(tal, bumtal):
-    if tal % bumtal == 0:
-        print(str(tal) + ' True')
-    elif str(bumtal) in str(tal):
-        print(str(tal) + ' True')
+gamemodeOne = input('Vil du selv spille?(Ja/Nej) ')
+
+if gamemodeOne == 'Ja':
+    gamemodeTwo = input('Hvor mange personer?(1/2) ')
+
+    if gamemodeTwo == '1':
+        print(1)
+    elif gamemodeTwo == '2':
+        print(2)
     else:
-        print(str(tal) + ' False')
+        print('Fejl, prøv igen')
 
-bumtal = int(input('Vælg et bumtal '))
+elif gamemodeOne == 'Nej':
 
-for tal in range(1, 51):
-    checkForBum(tal, bumtal)
+    def checkForBum(tal, bumtal):
+        if tal % bumtal == 0:
+            print(str(tal) + ' True')
+        elif str(bumtal) in str(tal):
+            print(str(tal) + ' True')
+        else:
+            print(str(tal) + ' False')
+
+    bumtal = int(input('Vælg et bumtal '))
+
+    for tal in range(1, 51):
+        checkForBum(tal, bumtal)
+
+else:
+    print('Fejl, prøv igen')
