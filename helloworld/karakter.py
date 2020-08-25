7028a122e08be06fc9b8434a75b803aa2601820e
sebwelsch <sebjak1@hotmail.com>
2020-08-25 08:55:39 +0200

    Create karakter.py

---
+++ helloworld/karakter.py
@@ -0,0 +1,10 @@
+karakter = {'Programmering':12, 'Dansk':10, 'Kom/IT':7, 'Idehistorie':4, 'Psykologi':2, 'Teknik':0}
+
+def udskrivEnKarakter(fag, bog):
+    if fag in bog.keys():
+        print('Karakter i {}: {}'.format(fag, bog[fag]))
+    else:
+        print('Fejl. Der findes ingen karakter for {} i den angivne karakterbog.'.format(fag))
+
+udskrivEnKarakter('Programmering', karakter)
+udskrivEnKarakter('Matematik', karakter)
