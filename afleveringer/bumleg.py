f18d522a5bdfa00c41f33f66affa25e7b1dbcc0e
sebwelsch <sebjak1@hotmail.com>
2020-08-26 23:35:33 +0200

    Commit

--- afleveringer/bumleg.py
+++ afleveringer/bumleg.py
@@ -1,29 +1,49 @@
-gamemodeOne = input('Vil du selv spille?(Ja/Nej) ')
+x = True
 
-if gamemodeOne == 'Ja':
-    gamemodeTwo = input('Hvor mange personer?(1/2) ')
-
-    if gamemodeTwo == '1':
-        print(1)
-    elif gamemodeTwo == '2':
-        print(2)
-    else:
-        print('Fejl, prøv igen')
-
-elif gamemodeOne == 'Nej':
-
-    def checkForBum(tal, bumtal):
-        if tal % bumtal == 0:
-            print(str(tal) + ' True')
-        elif str(bumtal) in str(tal):
-            print(str(tal) + ' True')
-        else:
-            print(str(tal) + ' False')
-
-    bumtal = int(input('Vælg et bumtal '))
-
-    for tal in range(1, 51):
-        checkForBum(tal, bumtal)
-
-else:
+def tryAgain():
     print('Fejl, prøv igen')
+
+def checkForBum(tal, bumtal):
+    if tal % bumtal == 0:
+        print('BUM!')
+        return True
+    elif str(bumtal) in str(tal):
+        print('BUM!')
+        return True
+    else:
+        print(str(tal))
+        return False
+
+while x:
+    gamemodeOne = input('Vil du selv spille?(Ja/Nej) ')
+    if gamemodeOne == 'Ja':
+
+        gamemodeTwo = input('Hvor mange spillere?(1/2) ')
+        if gamemodeTwo == '1':
+            x = False
+            print(1)
+        elif gamemodeTwo == '2':
+            x = False
+
+            bumtal = int(input('Vælg et bumtal '))
+
+            for tal in range(1, 1000):
+                checkForBum(tal, bumtal)
+
+        else:
+            tryAgain()
+
+    elif gamemodeOne == 'Nej':
+
+        x = False
+
+        print()
+        print('Computeren spiller nu selv')
+
+        bumtal = int(input('Vælg et bumtal '))
+
+        for tal in range(1, 51):
+            checkForBum(tal, bumtal)
+
+    else:
+        tryAgain()
